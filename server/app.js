d57ac05b473099c015a9dd8d27a987595caeff27
a-camarillo <anthony.camarillo.96@gmail.com>
2022-08-17 19:27:28 -0700

    main express file

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+// import routes
+const tasks = require('./routes/tasks');
+const projects = require('./routes/projects');
+
+app.use('/tasks', tasks);
+app.use('/projects', projects);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('This is my express server');
+});
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+});
