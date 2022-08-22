d4448348fa155806b34f165a72f8af6144adfc83
a-camarillo <anthony.camarillo.96@gmail.com>
2022-08-21 20:13:31 -0700

    modified database functions to match imports

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -4,25 +4,25 @@ import { query as db } from '../db/index.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let { rows } = await db.query('SELECT * FROM projects')
+    let { rows } = await db('SELECT * FROM projects')
     res.send(rows)
 });
 
 router.get('/:id', async (req, res) => {
     let { id } = req.params
-    let { rows } = await db.query('SELECT * FROM projects WHERE id = $1', [id])
+    let { rows } = await db('SELECT * FROM projects WHERE id = $1', [id])
     res.send(rows[0])
 });
 
 router.get('/:id/tasks/', async (req, res) => {
     let project_id = req.params.id
-    let { rows } = await db.query('SELECT * FROM tasks WHERE project_id = $1', [project_id])
+    let { rows } = await db('SELECT * FROM tasks WHERE project_id = $1', [project_id])
     res.send(rows)
 });
 
 router.post('/', async (req, res) => {
     let title = req.body
-    let { rows } = await db.query('INSERT INTO products (title) VALUES ($1)', [title])
+    let { rows } = await db('INSERT INTO products (title) VALUES ($1)', [title])
     res.send(rows[0])
 });
 
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,13 +4,13 @@ import { query as db } from '../db/index.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let { rows } = await db.query('SELECT * FROM tasks')
+    let { rows } = await db('SELECT * FROM tasks')
     res.send(rows)
 });
 
 router.get('/:id', async (req, res) => {
     let { id } = req.params
-    let { rows } = await db.query('SELECT * FROM tasks WHERE id = $1', [id])
+    let { rows } = await db('SELECT * FROM tasks WHERE id = $1', [id])
     res.send(rows[0])
 });
 
