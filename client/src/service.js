853342506ea9ae2bec1063f2c6698dfcd8a424c1
a-camarillo <anthony.camarillo.96@gmail.com>
2022-08-30 21:16:29 -0700

    converted functions to arrow functions for consistency and added get request for tasks of projects

--- client/src/service.js
+++ client/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function getTasks() {
+const getTasks = async() => {
     try {
         const response = await axios.get('http://localhost:5000/tasks');
         console.log(response);
@@ -10,7 +10,7 @@ async function getTasks() {
     }
 }
 
-async function getProjects() {
+const getProjects = async() => {
     try {
         const response = await axios.get('http://localhost:5000/projects');
         console.log(response);
@@ -20,4 +20,39 @@ async function getProjects() {
     }
 }
 
-export { getTasks, getProjects };
+const getProjectTasks = async(projectId) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/projects/${projectId}/tasks`);
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const postProject = async(title) => {
+    console.log(title)
+    try {
+        const response = await axios.post('http://localhost:5000/projects',{
+            title
+        })
+        window.location.reload();
+        console.log(response)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const deleteProject= async(projectId) => {
+    console.log(projectId)
+    try {
+        const response = await axios.delete(`http://localhost:5000/projects/${projectId}`)
+        window.location.reload();
+        console.log(response)
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+export { getTasks, getProjects, postProject, deleteProject, getProjectTasks };
