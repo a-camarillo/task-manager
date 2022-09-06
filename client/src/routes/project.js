baa97f6a4725373b59fd6a547921eebe1977bdf0
a-camarillo <anthony.camarillo.96@gmail.com>
2022-09-05 20:20:31 -0700

    refactored project component to include react bootstrap components and modal for adding tasks to project

--- client/src/routes/project.js
+++ client/src/routes/project.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { getProject, getProjectTasks, postTask } from '../service';
 import { useParams } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Task from '../components/task';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const Project = () => {
 
+    // Access the url parameters to get the project ID
     let { id } = useParams();
 
+    // GET request to the API projects endpoint and set the project state with the response
     const [project, setProject] = useState({});
     useEffect(() => {
         getProject(id).then((response) => {
@@ -13,6 +22,7 @@ const Project = () => {
         })
     }, []);
 
+    // GET request to the API tasks endpoint and set the tasks state with tasks based on id parameter
     const [tasks, setTasks] = useState([]);
     useEffect(() => {
         getProjectTasks(id).then((response) => {
@@ -20,6 +30,7 @@ const Project = () => {
         })
     }, []);
 
+    // POST request to the API tasks endpoint 
     const submitTask = (e) => {
         e.preventDefault();
         const projectId = id
@@ -27,26 +38,56 @@ const Project = () => {
         postTask(description, projectId);
     }
 
+    // logic for Modal 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div>
+        <Container>
             <h1 key={project.id}>
                 {project.title}
             </h1>
-            <button>Create New Task</button>
-            <form onSubmit={submitTask}>
-                <label>
-                    New Task:
-                    <input type="text" name="description"/>
-                </label>
-                <input type="submit" name="Submit"/>
-            </form>
-            {tasks.map(task => (
-                <div key={task.id}>
-                    <p>{task.description}</p>
-                    <p>{new Date(task.created_at).toLocaleDateString()}</p>
-                </div>
-            ))}
-        </div>
+            <Button variant="primary" onClick={handleShow}>
+                Create New Task
+            </Button>
+            <Modal show={show} 
+            onHide={handleClose} 
+            aria-labelledby="contained-modal-title-vcenter" 
+            size="lg"
+            centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create A New Task</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                        <Form onSubmit={submitTask}>
+                            <Form.Group className="mb-3" controlId="formDescription">
+                                <Form.Label>Task Description</Form.Label>
+                                <Form.Control 
+                                as="textarea"
+                                name="description" 
+                                placeholder="Please enter a description for this task."
+                                autoFocus
+                                required
+                                />
+                            </Form.Group>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="success" type="submit">
+                                Save Task
+                            </Button>
+                        </Form>
+                </Modal.Body>
+            </Modal>
+            <Row className="justify-content-md-center">
+                {tasks.map(task => (
+                    <Col key={task.id}>
+                        <Task task={task}/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
